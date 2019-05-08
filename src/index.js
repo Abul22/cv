7ba317bb578603134971e184ebab7ee6f3116b15
Simone <simone@duiker101.net>
2019-05-08 11:05:17 +0200

    added analytics and upgraded packages

--- src/index.js
+++ src/index.js
@@ -8,3 +8,15 @@ toggle.onclick = (e) => {
     else
         classes.add('collapsed')
 }
+
+// this is my personal analytics and it's fully anonymous.
+var _paq = window._paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//log.duiker101.net/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
